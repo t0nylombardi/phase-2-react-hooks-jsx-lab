4c556e428722b1ec1b233c69fda01e85a76d11bb
t0nylombardi <alombardi.331@gmail.com>
2024-05-08 13:31:25 -0400

    add jsx to components

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id='about'>
+    <h2>About Me</h2>
+    <p>I love to code. I am a web developer. I am a coder.</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
